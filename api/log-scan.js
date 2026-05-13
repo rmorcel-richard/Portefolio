100d3ac449708da3168c8c9c3137c563fcb2600e
rmorcel-richard <rmorcel@gmail.com>
2026-05-13 09:49:45 +0200

    Update log-scan.js

--- api/log-scan.js
+++ api/log-scan.js
@@ -1,3 +1,12 @@
 export default function handler(req, res) {
   if (req.method !== "POST") {
-    return res
+    return res.status(200).json({ ok: true });
+  }
+
+  console.log("QR SCAN:", req.body);
+
+  return res.status(200).json({
+    success: true,
+    time: new Date().toISOString()
+  });
+}
